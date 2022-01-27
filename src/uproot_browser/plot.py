3abaf149fd8adf00c23b20ca23afac623f2552f9
Henry Schreiner <henryschreineriii@gmail.com>
2022-01-27 15:41:12 -0500

    chore: updating checking and fix style issues

--- src/uproot_browser/plot.py
+++ src/uproot_browser/plot.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-import uproot
 import functools
-import plotext as plt
-import hist
-import numpy as np
+from typing import Any
+
 import awkward as ak
+import hist
+import plotext as plt
+import uproot
 
 
 @functools.singledispatch
@@ -16,9 +17,9 @@ def plot(tree: Any, command: str = "") -> None:
 @plot.register
 def plot_branch(tree: uproot.TBranch, command: str = "") -> None:
     array = tree.array()
-    h = hist.numpy.histogram(ak.flatten(array) if array.ndim > 1 else array, bins=50, histogram=hist.Hist)
+    h = hist.numpy.histogram(
+        ak.flatten(array) if array.ndim > 1 else array, bins=50, histogram=hist.Hist
+    )
     plt.clear_figure()
     plt.bar(h.axes[0].centers, h.values().astype(float))
     plt.show()
-
-
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import uproot_browser as m
 
 
