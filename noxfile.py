489b2f9f8431161d69040c4a48c404a3931bad1b
Aman Goel <aman.goel185@gmail.com>
2022-03-16 02:11:23 +0530

    docs: remove docs directory and related code

--- noxfile.py
+++ noxfile.py
@@ -35,24 +35,6 @@ def pylint(session: nox.Session) -> None:
 
 
 @nox.session
-def docs(session: nox.Session) -> None:
-    """
-    Build the docs. Pass "serve" to serve.
-    """
-
-    session.install(".[docs]")
-    session.chdir("docs")
-    session.run("sphinx-build", "-M", "html", ".", "_build")
-
-    if session.posargs:
-        if "serve" in session.posargs:
-            print("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
-            session.run("python", "-m", "http.server", "8000", "-d", "_build/html")
-        else:
-            print("Unsupported argument to docs")
-
-
-@nox.session
 def build(session: nox.Session) -> None:
     """
     Build an SDist and wheel.
