5065734ffcb92393937f0857144394350d0fac64
Henry Schreiner <henryschreineriii@gmail.com>
2023-05-08 17:12:24 -0400

    tests: add textual
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

---
+++ tests/test_tui.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import skhep_testdata
+
+from uproot_browser.tui.browser import Browser
+
+
+async def test_browse_logo() -> None:
+    async with Browser(
+        Path(skhep_testdata.data_path("uproot-Event.root"))
+    ).run_test() as pilot:
+        assert pilot.app.query_one("#main-view").current == "logo"
+
+
+async def test_browse_plot() -> None:
+    async with Browser(
+        Path(skhep_testdata.data_path("uproot-Event.root"))
+    ).run_test() as pilot:
+        await pilot.press("down", "down", "down", "enter")
+        assert pilot.app.query_one("#main-view").current == "plot"
+
+
+async def test_browse_empty() -> None:
+    async with Browser(
+        Path(skhep_testdata.data_path("uproot-empty.root"))
+    ).run_test() as pilot:
+        await pilot.press("down", "space", "down", "enter")
+        assert pilot.app.query_one("#main-view").current == "empty"
+
+
+async def test_browse_empty_vim() -> None:
+    async with Browser(
+        Path(skhep_testdata.data_path("uproot-empty.root"))
+    ).run_test() as pilot:
+        await pilot.press("j", "l", "j", "enter")
+        assert pilot.app.query_one("#main-view").current == "empty"
